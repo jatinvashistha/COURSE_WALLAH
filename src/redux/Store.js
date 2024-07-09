a51c3fac4715259d57803f9caa4ed6a394ab1e40
Jatin Vashisth <136696926+jatinvashistha@users.noreply.github.com>
2024-07-09 19:35:54 +0530

    Update Store.js

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -20,4 +20,4 @@ const store = configureStore({
 });
 export default store;
 
-export const server = 'https://lerarn-with-jatin.onrender.com/api/v1';
+export const server = 'https://8mmn98-4000.csb.app/api/v1';
